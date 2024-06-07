2c53646147d68e9db16e9a86b44431b99f9f7350
hbmubin <hasanulbannamubin01@gmail.com>
2024-06-07 18:45:34 +0600

    recent-review-&-property-details-added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jweumb2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,6 +32,31 @@ async function run() {
 
     app.get("/verified-properties", async (req, res) => {
       const result = await propertiesCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/advertised-properties", async (req, res) => {
+      const query = { advertised: "true" };
+      const result = await propertiesCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/recent-reviews", async (req, res) => {
+      const pipeline = [
+        { $unwind: "$reviews" },
+        { $sort: { "reviews.timestamp": -1 } },
+        { $limit: 4 },
+        { $project: { _id: 0, review: "$reviews" } },
+      ];
+      const recentReviews = await propertiesCollection
+        .aggregate(pipeline)
+        .toArray();
+      res.send(recentReviews.map((item) => item.review));
+    });
+
+    app.get("/property/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await propertiesCollection.findOne(query);
       res.send(result);
     });
 
